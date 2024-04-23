f709d6cae837c9ba948cde3f65bca6d3b8857747
Xavier Mercerweiss <xfrancismw@proton.me>
2024-04-23 15:20:30 -0600

    Created basic panels

--- src/gui/panels.py
+++ src/gui/panels.py
@@ -1,11 +1,39 @@
-
-class ToolPanel:
-    pass
-
-
-class MajorPanel:
-    pass
+import tkinter as tk
+from abc import ABC
 
 
-class MinorPanel:
-    pass
+class Panel(ABC):
+
+    _ATTR_ERR_MSG = "No attribute {} found in class {}"
+
+    def __init__(self, parent):
+        self._frame = tk.Frame(parent)
+
+    def __getattr__(self, item):
+        try:
+            attr = getattr(self._frame, item)
+            return attr
+        except AttributeError:
+            msg = self._ATTR_ERR_MSG.format(str(item), self.__class__.__name__)
+            raise AttributeError(msg)
+
+
+class ToolPanel(Panel):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self._frame.configure(bg="red")
+
+
+class MajorPanel(Panel):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self._frame.configure(bg="blue")
+
+
+class MinorPanel(Panel):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure(bg="green")
--- src/gui/windows.py
+++ src/gui/windows.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 
 import utils.customio as ioutil
+from . import panels
 
 
 class RootWindow:
@@ -14,12 +15,20 @@ class RootWindow:
         self._config_path = config
         self._config = ioutil.read_key_value_file(config)
         self._root = self._init_root()
+        self._tool_panel = self._init_panel()
 
     def _init_root(self):
         root = tk.Tk()
         root.title(self._config[self._TITLE_KEY])
         root.geometry(self._config[self._GEOMETRY_KEY])
+        root.rowconfigure(0, weight=1)
+        root.columnconfigure(0, weight=1)
         return root
+
+    def _init_panel(self):
+        panel = panels.ToolPanel(self._root)
+        panel.grid(row=0, column=0, sticky=tk.NSEW)
+        return panel
 
     def launch(self):
         self._root.mainloop()
